74e19835b884c824fc5a308df8ddccef14d83456
xPamii <pbherath2018@gmail.com>
2026-01-25 09:52:00 +0530

    feat: Add TabLayout and ViewPager2 for Sign In/Sign Up
    Integrates `TabLayout` and `ViewPager2` into `MainActivity` to create a tabbed interface for "Sign In" and "Sign Up" screens.
    
    - A `ViewPagerAdapter` is set up to manage the fragments for each tab.
    - Listeners are implemented to synchronize the selected tab with the `ViewPager2`'s current page and vice versa.
    - The previous boilerplate code for handling window insets has been removed.

--- app/src/main/java/com/xpamii/chatapp_android/activity/MainActivity.java
+++ app/src/main/java/com/xpamii/chatapp_android/activity/MainActivity.java
@@ -7,20 +7,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
 import com.xpamii.chatapp_android.R;
+import com.xpamii.chatapp_android.adapter.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+
+    private ViewPagerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Sign Up"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sign In"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
         });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
+
     }
 }
